7147f9d8ecbcf4df2e1ea36ac0698399cfb3ede5
rbartelt <robert_bartelt@web.de>
2021-11-12 17:53:17 +0100

    added Buttom Navigation Bar https://www.youtube.com/watch?v=llWsm9Pjkpc

--- app/src/main/java/de/xxlstrandkorbverleih/navigation/MainActivity.java
+++ app/src/main/java/de/xxlstrandkorbverleih/navigation/MainActivity.java
@@ -15,20 +15,37 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //get NavHostFragment
         final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        navController = navHostFragment.getNavController();
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        NavigationUI.setupActionBarWithNavController(this, navController);
-        //NavigationUI.setupWithNavController(toolbar, navController);
+        navController = navHostFragment.getNavController();                         //get NavController
+
+        //set TopLevelDestinations
+        Set<Integer> topLevel = new HashSet<>();
+        topLevel.add(R.id.homeFragment);
+        topLevel.add(R.id.searchFragment);
+        //and set it to the AppBarConfiguration to avoid the Back Button shown in the actionBar
+        appBarConfiguration = new AppBarConfiguration.Builder(topLevel).build();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);                               //find Toolbar
+        setSupportActionBar(toolbar);                                               //set Toolbar as Actionbar in Mainactivity
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);   //connects the Toolbar with Navgraph
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);  //find BottomNavigationView
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);   //connects BottomNavigationView with NavGraph
     }
 
     @Override
---
+++ app/src/main/java/de/xxlstrandkorbverleih/navigation/SearchFragment.java
@@ -0,0 +1,9 @@
+package de.xxlstrandkorbverleih.navigation;
+
+import androidx.fragment.app.Fragment;
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {super(R.layout.fragment_search);}
+
+}
